b7d82fe8a0bd6d5029b6494fc320d9116d0bcaa5
Matt <mwight1986@gmail.com>
2017-02-17 12:37:23 -0600

    on task 3

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
 
 document.querySelector("#thanks button").addEventListener('click',function(){
   // TASK #1
+  alert('Yowch! Dont click me so hard!');
 })
 
 document.querySelector("#double button").addEventListener('click',function(){
   // TASK #2
+  var num = document.querySelector('#compoundInvestment');
+  var numHTML = num.innerHTML
+  console.log(numHTML)
+  num.innerHTML = numHTML*2
 })
 
 
 document.querySelector("#color-circle button").addEventListener('click',function(){
   // TASK #3
+  var circle = document.querySelector('#circle-bw');
+  var c1 = {
+  	white : true
+  }
+  var turnBlack = function(){
+  	circle.style.backgroundColor ='black';
+  	c1['white'] = false;
+  	console.log(c1.white);
+  }
+  var turnWhite = function(){
+  	circle.style.backgroundColor ='white';
+  	c1['white'] = true;
+  if(c1.white === tru
+  	console.log(c1)
+  }e){
+  	turnBlack();
+  }
+  else {
+  	console.log('ok')
+  	turnWhite();
+  }
+
 })
 
 document.querySelector("#blow-up button").addEventListener('click',function(){
